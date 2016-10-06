0f2959fed7d7874dab8628bea8139d97bab7e584
Massimo Biagioli <m.biagioli@palinformatica.it>
2016-10-06 14:37:48 +0200

    Aggiunta gestione configurazioni

--- 05-Libs/application/config/autoload.php
+++ 05-Libs/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array(
+$autoload['libraries'] = array(    
     'html_lib'
 );
 
@@ -99,7 +99,9 @@ $autoload['helper'] = array();
 | config files.  Otherwise, leave it blank.
 |
 */
-$autoload['config'] = array();
+$autoload['config'] = array(
+    'render'
+);
 
 /*
 | -------------------------------------------------------------------
--- 05-Libs/application/libraries/Html_lib.php
+++ 05-Libs/application/libraries/Html_lib.php
@@ -2,9 +2,21 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Html_lib {
-
+    
+    private $CI;
+    
+    public function __construct() {
+        $this->CI = &get_instance();;
+    }
+    
     public function render_span($element) {
-        return '<span style="' . $element['style'] . '">' . $element['name'] . '</span>';
+        switch ($this->CI->config->item('render_lib')) {
+            case 'withStyle':
+                return '<span style="' . $element['style'] . '">' . $element['name'] . '</span>';
+            case 'withoutStyle':
+            default:
+                return '<span>' . $element['name'] . '</span>';                
+        }                
     }
     
 }
